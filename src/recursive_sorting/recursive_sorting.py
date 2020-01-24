3bc76058df43bede3c6c342523858c0c9dd430ab
Kristin Barr <kristinbarr@gmail.com>
2020-01-24 01:08:19 -0500

    merge sort complete with notes

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,18 +1,32 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    total_elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
+def merge(left, right):
+    total_elements = len(left) + len(right)
+    merged_arr = [0] * total_elements
     # TO-DO
+    # create indexes to iterate through each side's
+    a = 0
+    b = 0
 
     # starting at beginning of `a` and `b`
-    # compare the next value of each
-    if arrA[0] <= arrB[0]:
-        # add smallest to `merged_arr`
-        merged_arr.append(arrA[0])
-        merged_arr.append(arrB[0])
-    else:
-        merged_arr.append(arrB[0])
-        merged_arr.append(arrA[0])
+    for i in range(0, total_elements):
+
+        if a >= len(left): # if there are no more elements in left side
+            merged_arr[i] = right[b] # merge from right side
+            b += 1
+
+        elif b >= len(right): # if no elements in right side
+            merged_arr[i] = left[a] # merge from left side
+            a += 1
+
+        elif left[a] < right[b]: # check 1st el in lists for greater el
+            merged_arr[i]= left[a]
+            a += 1
+
+        else: # else case if right element is greater then left
+            merged_arr[i] = right[b]
+            b += 1
+        # compare the next value of each
+            # add smallest to `merged_arr`
 
     print('HELPER MERGED ARR:', merged_arr)
     return merged_arr
@@ -26,20 +40,14 @@ def merge_sort( arr ):
         left = arr[:len(arr) // 2] # slice to get left half
         right = arr[len(arr) // 2:] # slice to get right half
 
-
-
         # recursively call merge_sort() on LHS
+        print("left", left)
+        left = merge_sort(left)
         # recursively call merge_sort() on RHS
+        print("right", right)
+        right = merge_sort(right)
         # merge sorted pieces
-
-    # divide array in half until there are subarrays of length 1
-    # compare smaller left values of pairs
-    # move smaller value to new array
-    # compare smaller left values of pairs again
-    # move smaller value to new array
-    # when only one value, add that array to the end of the new arr
-    # merge sorted lists together
-
+        return merge(left, right)
     return arr
 
 
