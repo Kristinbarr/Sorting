9656fa924c1cdf235c6f4c478d9a16e9dfa8a7e5
Kristin Barr <kristinbarr@gmail.com>
2020-02-20 15:06:26 -0500

    Jacks merge sort in place

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -65,15 +65,46 @@ def merge_sort( arr ):
 print(merge_sort([2, 1, 5, 4, 3]))
 
 # STRETCH: implement an in-place merge sort algorithm
-def merge_in_place(arr, start, mid, end):
-    # TO-DO
-
-    return arr
-
+def merge_in_place(arr, start, mid, end): 
+    start2 = mid + 1; 
+    # If the direct merge is already sorted 
+    if (arr[mid] <= arr[start2]): 
+        return
+    # Two pointers to maintain start 
+    # of both arrays to merge 
+    while (start <= mid and start2 <= end): 
+        # If element 1 is in right place 
+        if (arr[start] <= arr[start2]): 
+            start += 1; 
+        else: 
+            value = arr[start2]; 
+            index = start2; 
+            # Shift all the elements between element 1 
+            # element 2, right by 1. 
+            while (index != start): 
+                arr[index] = arr[index - 1]; 
+                index -= 1; 
+            arr[start] = value; 
+            # Update all the pointers 
+            start += 1; 
+            mid += 1; 
+            start2 += 1; 
+    return
+''' 
+* l is for left index and r is right index of 
+the sub-array of arr to be sorted 
+'''
 def merge_sort_in_place(arr, l, r): 
-    # TO-DO
+    if (l < r): 
+        # Same as (l + r) / 2, but avoids overflow 
+        # for large l and r 
+        m = l + (r - l) // 2
+        print(l,r)
+        # Sort first and second halves 
+        merge_sort_in_place(arr, l, m); 
+        merge_sort_in_place(arr, m + 1, r); 
+        merge_in_place(arr, l, m, r); 
 
-    return arr
 
 
 # STRETCH: implement the Timsort function below
